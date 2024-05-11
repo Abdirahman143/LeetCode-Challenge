2ac31608fa2e9da2b7f0ba7a8fc1e916d1306501
Abdirahman <bashirabdi1994@gmail.com>
2024-05-11 19:08:45 +0300

    adding sum two challenge

--- src/main/java/com/leetCode/easy/TwoSum.java
+++ src/main/java/com/leetCode/easy/TwoSum.java
@@ -1,6 +1,41 @@
 package com.leetCode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *input
+ * output
+ * logic/pseudo code
+ * 1. create map
+ * 2. loop
+ * 3. declare var hold current element
+ * 4. I get the diff between target and each element
+ * 5. check if the map key contains the element
+ * if it contains then return the index, compliment index
+ * if not then push the key to the map for subsequent checks
+ * test
+ * run you test
+ *
+ *
+ */
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer>addTarget =new HashMap<>();
+        for(int i=0 ; i<nums.length; i++){
+            int current =nums[i];
+            int compliment = target-current;
+            if(addTarget.containsKey(compliment)){
+                return new int[]{i,addTarget.get(compliment)};
+            }
+            addTarget.put(current,i);
+        }
+        return new int[]{};
+    }
+
+
+
 
 
 }
--- src/test/java/com/leetCode/easy/TwoSumTest.java
+++ src/test/java/com/leetCode/easy/TwoSumTest.java
@@ -1,14 +1,53 @@
 package com.leetCode.easy;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TwoSumTest {
 
+       private  TwoSum solution ;
+       private int target1 =9;
+       private int target2 =6;
+       private int arr1[]=new int[]{2,7,11,15};
+       private int arr2[]=new int[]{3,2,4};
+       private  int arr3[]=new int[]{3,3};
+            @BeforeEach
+        public void setUp(){
+            solution  = new TwoSum();
+
+        }
 
 
+    @Test
+    @Order(1)
+    @DisplayName("TC: 1")
+    public void testScenarioOne() {
+        assertArrayEquals(new int[]{1, 0}, solution.twoSum(arr1, target1),
+                "The indices of the elements that add up to 9 should be [1, 0].");
+
+        }
+
+    @Test
+    @Order(2)
+    @DisplayName("TC:2 ")
+    public void testScenarioTwo() {
+                int[]expected =new int[]{1,2};
+                int[] actual =solution.twoSum(arr2,target1);
+                assertFalse(Arrays.equals(expected,actual));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("TC:3")
+    public void testScenarioThree(){
+                int [] expected =new int[]{0,1};
+                int [] actual = solution.twoSum(arr3,target2);
+                assertArrayEquals(expected,actual);
+    }
 
 
 
